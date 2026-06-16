dc14df1a298821d86cfa56af309f7390f0f061f7
gabrielalcacer <145050595+gabrielalcacer@users.noreply.github.com>
2026-06-16 10:06:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,11 @@
-const CACHE = 'plan3d-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+// Service worker minimo - sin cache para evitar problemas
+self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
-  );
+  e.respondWith(fetch(e.request));
 });
